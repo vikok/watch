e37d3ae5e1ea16445d62772ff0f0cddc21410c0b
vikok <vikskopkop@gmail.com>
2017-01-04 12:30:01 +0200

    add all

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 ;(function($) {
 	"use strict";
 
+	var $fullScreenSlider = $('.ba-slider');
+	$fullScreenSlider.slick({
+		dots: true,
+		vertical: true,
+		verticalSwiping: true
 
+	});
 
 })(jQuery);
